8d8c20c992170f7bca6d656f4cafc43bf59a2706
Shubham Sharma <coolshubhamsharma26@gmail.com>
2025-05-28 14:51:09 +0530

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import AboutUs from "./pages/AboutUs"
 import Blog from "./pages/Blog"
@@ -8,24 +8,28 @@ import AllBlogs from "./pages/AllBlogs"
 import YogaClasses from "./pages/yoga classes/YogaClasses"
 import YogaClassesHome from "./pages/YogaClassesHome"
 import ShowBlog from "./pages/yoga classes/ShowBlog"
+import ScrollTop from "./components/Whatsapp/ScrollTop"
 // import Admin from "./pages/Admin"
 
 
 const App = () => {
   return (
     <div className="">
-      <Routes>
-        <Route path="/"  element={<Home/>} />
-        <Route path="/about-us"  element={<AboutUs/>} />
-        <Route path="/blog"  element={<Blog/>} />
-        <Route path="/contact-us"  element={<ContactUs/>} />
-        <Route path="/write-for-us"  element={<WriteForUs/>} />
-        <Route path="/allblogs"  element={<AllBlogs/>} />
-        <Route path="/yoga-classes"  element={<YogaClasses/>} />
-        <Route path="/home-yoga-classes/:city"  element={<YogaClassesHome/>} />
-        <Route path="/showblog"  element={<ShowBlog/>} />
-        {/* <Route path="/admin"  element={<Admin/>} /> */}
-      </Routes>
+      
+        <ScrollTop/>
+        <Routes>
+          <Route path="/"  element={<Home/>} />
+          <Route path="/about-us"  element={<AboutUs/>} />
+          <Route path="/blog"  element={<Blog/>} />
+          <Route path="/contact-us"  element={<ContactUs/>} />
+          <Route path="/write-for-us"  element={<WriteForUs/>} />
+          <Route path="/allblogs"  element={<AllBlogs/>} />
+          <Route path="/yoga-classes"  element={<YogaClasses/>} />
+          <Route path="/home-yoga-classes/:city"  element={<YogaClassesHome/>} />
+          <Route path="/showblog"  element={<ShowBlog/>} />
+          {/* <Route path="/admin"  element={<Admin/>} /> */}
+        </Routes>
+      
     </div>
   )
 }
--- src/components/ContactUs/GetInTouch.tsx
+++ src/components/ContactUs/GetInTouch.tsx
@@ -1,12 +1,26 @@
 
 import { useForm, ValidationError } from '@formspree/react';  
+import { useEffect, useRef } from 'react';
 
 const GetInTouch = () => {
 
   const [state, handleSubmit] = useForm("xeoavzpn");
-  if (state.succeeded) {
-      return <p>"</p>;
-  }
+
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const subjectRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (state.succeeded) {
+      alert("Thank you for contacting me, I will get back to you ASAP!");
+
+      if (nameRef.current) nameRef.current.value = '';
+      if (emailRef.current) emailRef.current.value = '';
+      if (subjectRef.current) subjectRef.current.value = '';
+      if (messageRef.current) messageRef.current.value = '';
+    }
+  }, [state.succeeded]);
 
   return (
     <div>
@@ -20,7 +34,7 @@ const GetInTouch = () => {
           <form onSubmit={handleSubmit} action="https://formspree.io/f/xeoavzpn" method='POST' className=" ml-6 mt-2">
             <fieldset className="fieldset">
               <label htmlFor='name' className="fieldset-legend text-lg text-gray-800">Your Name </label>
-              <input type="text" className="input" placeholder="Name" id='name' name='name'/>
+              <input type="text" className="input" placeholder="Name" id='name' name='name' ref={nameRef}/>
               <ValidationError 
               prefix="Name" 
               field="name"
@@ -30,7 +44,7 @@ const GetInTouch = () => {
 
             <fieldset className="fieldset mt-1">
               <label htmlFor='email' className="fieldset-legend text-lg text-gray-800">Your Email </label>
-              <input type="email" id='email' name='email' className="input" placeholder="Email" />
+              <input type="email" id='email' name='email' className="input" placeholder="Email" ref={emailRef}/>
               <ValidationError 
               prefix="Email" 
               field="email"
@@ -40,7 +54,7 @@ const GetInTouch = () => {
 
             <fieldset className="fieldset mt-1">
               <label htmlFor='subject' className="fieldset-legend text-lg text-gray-800">Subject </label>
-              <input type="text" id='subject' className="input" name='subject' placeholder="Type here" />
+              <input type="text" id='subject' className="input" name='subject' placeholder="Type here" ref={subjectRef}/>
               <ValidationError 
               prefix="Subject" 
               field="subject"
@@ -50,7 +64,7 @@ const GetInTouch = () => {
 
             <fieldset className="fieldset mt-1">
               <label htmlFor='message' className="fieldset-legend text-lg text-gray-800">Your Message </label>
-              <textarea id='message' name='message' className="input h-[30vh]" placeholder="Type here" />
+              <textarea id='message' name='message' className="input h-[30vh]" placeholder="Type here" ref={messageRef}/>
               <ValidationError 
               prefix="Message" 
               field="message"
